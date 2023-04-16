176b4f7ec768c02828fb28a1b77833efb267258d
ElenaMUSCEL <124464514+ElenaMUSCEL@users.noreply.github.com>
2023-04-16 15:07:30 +0300

    Update

---
+++ src/main/java/ro/challenge/Challange/Api/ChallangeApiApplication.java
@@ -0,0 +1,13 @@
+package ro.challenge.Challange.Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallangeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallangeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/challenge/Challange/Api/ChallangeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.challenge.Challange.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallangeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
